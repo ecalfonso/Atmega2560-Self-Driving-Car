da79a5c1405505192b706b96dfb89499e81ccf80
ecalfonso <8077056+ecalfonso@users.noreply.github.com>
2018-04-14 23:41:27 -0700

    Create Function for Car Movement
    Make a For loop that iterates through each motor.
    
    We know the definition of FORWARD, BACKWARD, BREAK, so it's simple to assign which wheel to do what.
    
    Use bitwise operators to modify predetermined variables for assigning motor direction.

--- main.ino/main.ino.ino
+++ main.ino/main.ino.ino
@@ -14,11 +14,39 @@
 #define FL_SENSOR 14
 #define FR_SENSOR 15
 
+/* 
+ *  Define Car Maneuvers
+ *  M1 - BACK LEFT
+ *  M2 - BACK RIGHT
+ *  M3 - FRONT RIGHT
+ *  M4 - FRONT LEFT
+ *  
+ *  FORWARD - 1
+ *  BACKWARD - 2
+ *  BREAKE - 3
+ *  RELEASE - 4
+ */
+#define GO_BACKWARD   0x2222
+#define GO_FORWARD    0x1111
+#define ROTATE_LEFT   0x2112
+#define ROTATE_RIGHT  0x1221
+#define TURN_LEFT     0x3113
+#define TURN_RIGHT    0x1331
+
 /*
 * Create MotorDriver
 * D0 - D13 and A0- A7 are used by the Shield
 */
 MotorDriver m;
+
+/*
+ * Set up quick functions for car maneuvering
+ */
+void moveCar(int mv) {
+  for (int i = 0; i < 4; i++) {
+    m.motor(i+1, ((mv >> (i*4)) & (0xF)), 255);
+  }
+}
 
 void setup() {
   // Setup Sensor Pins as inputs
@@ -27,5 +55,5 @@ void setup() {
 }
 
 void loop() {
-  m.motor(FL_WHEEL, FORWARD, 255);
+  
 }
